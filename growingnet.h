56e26e269aa655c2b20be5663d54b34bd2d4b365
Andrzej Sulecki <asulecki@gmail.com>
2013-10-31 14:10:33 +0100

    dodany growingnet.h

---
+++ growingnet.h
@@ -0,0 +1,12 @@
+#ifndef GROWINGNET_H
+#define GROWINGNET_H
+
+#include "network.h"
+
+
+#ifdef __cplusplus
+	extern "C" const unsigned long growingnet;
+#else
+	extern const unsigned long growingnet;
+#endif //__cplusplus
+#endif //GROWINGNET_H
